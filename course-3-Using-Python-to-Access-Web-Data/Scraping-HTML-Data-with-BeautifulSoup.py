0f481380c369b4415926f874485e2ce8c862cf1c
Afonso Carvalho <afonso.martingo.c@gmail.com>
2025-01-28 21:56:50 +0000

    Ex Solved

--- course-3-Using-Python-to-Access-Web-Data/Scraping-HTML-Data-with-BeautifulSoup.py
+++ course-3-Using-Python-to-Access-Web-Data/Scraping-HTML-Data-with-BeautifulSoup.py
@@ -33,3 +33,33 @@ $ python3 solution.py
 Enter - http://py4e-data.dr-chuck.net/comments_42.html
 Count 50
 Sum 2...'''
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
+
+total = 0
+count = 0
+
+tags = soup('span')
+for tag in tags:
+    # Look at the parts of a tag
+    # print('TAG:', tag)
+    # print('URL:', tag.get('href', None))
+    # print('Contents:', tag.contents[0])
+    # print('Attrs:', tag.attrs)
+    count += 1
+    total += int(tag.contents[0])
+
+print('Count', count)
+print('Sum', total)
+
